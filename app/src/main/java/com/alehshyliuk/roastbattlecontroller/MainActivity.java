15669a2cce117bb993bdaf66284dc615cab534b7
AlehShyliuk <47559090+AlehShyliuk@users.noreply.github.com>
2020-07-01 18:27:43 +0300

    Revert "Code refactoring."

--- app/src/main/java/com/alehshyliuk/roastbattlecontroller/MainActivity.java
+++ app/src/main/java/com/alehshyliuk/roastbattlecontroller/MainActivity.java
@@ -9,86 +9,62 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
 
-    private static final DataEntry[] buttons = new DataEntry[]{
-            new DataEntry("button1", "cock"),
-            new DataEntry("button2", "gong"),
-            new DataEntry("button3", "grenade"),
-            new DataEntry("button4", "gun"),
-            new DataEntry("button5", "haha"),
-            new DataEntry("button6", "horn")
-    };
+    private final List<ImageButton> playButtons = new ArrayList<>();
+    private final List<MediaPlayer> players = new ArrayList<>();
 
-    private final ButtonController[] buttonControllers = new ButtonController[buttons.length];
-
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        for (int ind = 0; ind < buttons.length; ++ind) {
-            buttonControllers[ind] = new ButtonController(this,
-                    buttons[ind],
-                    R.drawable.but_green,
-                    ind % 2 == 0 ? R.drawable.but_blue : R.drawable.but_red);
-        }
-    }
+        initializePlayers();
+        initializeButtons();
 
-    private static class DataEntry {
-        public String viewName, dataName;
+        for (final ImageButton btn: playButtons){
+            btn.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    int ind = playButtons.indexOf(btn);
 
-        public DataEntry(String viewName, String dataName) {
-            this.viewName = viewName;
-            this.dataName = dataName;
-        }
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    private static class ButtonController implements View.OnTouchListener {
-        private final ImageButton button;
-        private final MediaPlayer player;
-        private final int backgroundActiveId;
-        private final int backgroundInactiveId;
-
-        public ButtonController(Activity activity, DataEntry dataEntry, int backgroundActiveId, int backgroundInactiveId) {
-            this(activity, dataEntry.viewName, dataEntry.dataName, backgroundActiveId, backgroundInactiveId);
-        }
-
-
-        public ButtonController(Activity activity, String buttonName, String dataName, int backgroundActiveId, int backgroundInactiveId) {
-            this(activity,
-                    activity.getResources().getIdentifier(buttonName, "id", activity.getPackageName()),
-                    activity.getResources().getIdentifier(dataName, "raw", activity.getPackageName()),
-                    backgroundActiveId,
-                    backgroundInactiveId);
-        }
-
-        public ButtonController(Activity activity, int buttonId, int dataId, int backgroundActiveId, int backgroundInactiveId) {
-            this.button = (ImageButton) activity.findViewById(buttonId);
-            this.player = MediaPlayer.create(activity, dataId);
-            this.backgroundActiveId = backgroundActiveId;
-            this.backgroundInactiveId = backgroundInactiveId;
-            this.button.setOnTouchListener(this);
-        }
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                button.setBackgroundResource(backgroundActiveId);
-            } else {
-                button.setBackgroundResource(backgroundInactiveId);
-                if (player.isPlaying()) {
-                    player.pause();
+                    if (event.getAction() == MotionEvent.ACTION_DOWN)
+                        btn.setBackgroundResource(R.drawable.but_green);
+                    else if (event.getAction() == MotionEvent.ACTION_UP){
+                        btn.setBackgroundResource(ind % 2 == 0 ? R.drawable.but_blue : R.drawable.but_red);
+                        if (players.get(ind).isPlaying()){
+                            players.get(ind).pause();
+                        }
+                        players.get(ind).seekTo(0);
+                        players.get(ind).start();
+                    }
+                    return false;
                 }
-                player.seekTo(0);
-                player.start();
-            }
-
-            return false;
+            });
         }
+    }
+
+    private void initializeButtons() {
+        playButtons.add((ImageButton) this.findViewById(R.id.button1));
+        playButtons.add((ImageButton) this.findViewById(R.id.button2));
+        playButtons.add((ImageButton) this.findViewById(R.id.button3));
+        playButtons.add((ImageButton) this.findViewById(R.id.button4));
+        playButtons.add((ImageButton) this.findViewById(R.id.button5));
+        playButtons.add((ImageButton) this.findViewById(R.id.button6));
+    }
+
+    private void initializePlayers() {
+        players.add(MediaPlayer.create(this, R.raw.cock));
+        players.add(MediaPlayer.create(this, R.raw.gong));
+        players.add(MediaPlayer.create(this, R.raw.grenade));
+        players.add(MediaPlayer.create(this, R.raw.gun));
+        players.add(MediaPlayer.create(this, R.raw.haha));
+        players.add(MediaPlayer.create(this, R.raw.horn));
     }
 }
